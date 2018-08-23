d6f6efd83b16ec594dfc5f8b7666a1bdf1587e1e
jiamo <life.130815@gmail.com>
2018-08-23 19:44:32 +0800

    add a wrapper puller by Jay Deiman and make greenlet_concurrent_server work in MacOSX

--- greenlet_concurrent_server.py
+++ greenlet_concurrent_server.py
@@ -5,16 +5,32 @@ No loop No more api Just use greenlet
 """
 from contextlib import contextmanager
 from enum import Enum
+from util import poller
 import socket
 import io
 import sys
 import greenlet
+import time
 import select
 
+# This is a hack to make this work on systems where POLLIN and
+# POLLPRI (OS X, apparently...) are not defined in the select module
+if not hasattr(select, 'POLLIN') or not hasattr(select, 'POLLPRI') or not \
+        hasattr(select, 'POLLOUT'):
+    setattr(select, 'POLLIN', 1)
+    setattr(select, 'POLLPRI', 2)
+    setattr(select, 'POLLOUT', 4)
+    setattr(select, 'POLLERR', 8)
 
-class ProcessingState(Enum):
-    WAIT_FOR_MSG = 0
-    IN_MSG = 1
+
+if not hasattr(select, 'EPOLLIN') or not hasattr(select, 'EPOLLPRI') or not \
+            hasattr(select, 'EPOLLOUT'):
+
+    setattr(select, 'EPOLLIN', 1)
+    setattr(select, 'EPOLLPRI', 2)
+    setattr(select, 'EPOLLOUT', 4)
+    setattr(select, 'EPOLLERR', 8)
+
 
 # It seem need a new level manager
 
@@ -32,7 +48,8 @@ def socketcontext(*args, **kwargs):
 @contextmanager
 def epollcontext(*args, **kwargs):
     """Context manager for an epoll loop."""
-    e = select.epoll()
+    e = poller.get_poller(select.EPOLLIN | select.EPOLLOUT)
+
     e.register(*args, **kwargs)
     try:
         yield e
@@ -46,8 +63,11 @@ class GreenletManager():
     def __init__(self, back_greenlet, task_fun):
         self.back_greenlet = back_greenlet
         self.work_greenlet = greenlet.greenlet(task_fun)
+        self.conn = None
+        self.finished = False
 
     def start(self, conn, fd, epoll):
+        self.conn = conn
         self.work_greenlet.switch(self, conn, fd, epoll)
 
     def return_back(self):
@@ -55,6 +75,15 @@ class GreenletManager():
 
     def continue_work(self):
         self.work_greenlet.switch()
+
+    def finish(self):
+        self.finished = True
+        self.return_back()
+
+    def check_close(self):
+        if self.finished:
+            if not self.conn._closed:
+                self.conn.close()
 
 
 class ProcessingState(Enum):
@@ -89,46 +118,70 @@ sock_status_RW = SockStatus(True, True)
 sock_status_NORW = SockStatus(False, False)
 
 
-def do_register(peer_status, fd, epoll):
+def do_register(peer_status, sock, epoll):
 
     if peer_status == sock_status_RW:
         try:
-            epoll.register(fd, select.EPOLLIN | select.EPOLLOUT)
+            epoll.register(sock, select.EPOLLIN | select.EPOLLOUT)
         except FileExistsError:
-            epoll.modify(fd, select.EPOLLIN | select.EPOLLOUT)
+            epoll.modify(sock, select.EPOLLIN | select.EPOLLOUT)
 
     if peer_status == sock_status_R:
         try:
-            epoll.register(fd, select.EPOLLIN)
+            epoll.register(sock, select.EPOLLIN)
         except FileExistsError:
-            epoll.modify(fd, select.EPOLLIN)
+            epoll.modify(sock, select.EPOLLIN)
 
     if peer_status == sock_status_W:
         try:
-            epoll.register(fd, select.EPOLLOUT)
+            epoll.register(sock, select.EPOLLOUT)
         except FileExistsError:
-            epoll.modify(fd, select.EPOLLOUT)
+            epoll.modify(sock, select.EPOLLOUT)
 
     if peer_status == sock_status_NORW:
         try:
-            epoll.unregister(fd)
+            epoll.unregister(sock)
         except FileNotFoundError:
             pass
+
+
+def send(conn, data):
+    while True:
+        try:
+            conn.send(data)
+            break
+        except socket.error as e:
+            if str(e) == "[Errno 35] Resource temporarily unavailable":
+                continue
+            raise e
+
+
+def read(conn, n):
+    while True:
+        try:
+            result = conn.recv(n)
+            break
+        except socket.error as ex:
+            if str(ex) == "[Errno 35] Resource temporarily unavailable":
+                continue
+            raise ex
+
+    return result
 
 
 def serve_connection(manager, conn, fd, epoll):
     # need to know when to switch out
     # once it need io it switch to main
-    do_register(sock_status_W, fd, epoll)
+    do_register(sock_status_W, conn, epoll)
     manager.return_back()
-    conn.send(b"*")
+    send(conn, b"*")
     state = ProcessingState.WAIT_FOR_MSG
 
     while True:
         buffer_len = 1024
-        do_register(sock_status_R, fd, epoll)
+        do_register(sock_status_R, conn, epoll)
         manager.return_back()
-        data = conn.recv(buffer_len)
+        data = read(conn, buffer_len)
         data_str = data.decode()
         print(f"recv {data}")
         if not data:
@@ -142,11 +195,11 @@ def serve_connection(manager, conn, fd, epoll):
                 if ch == '$':
                     state = ProcessingState.WAIT_FOR_MSG
                 else:
-                    do_register(sock_status_W, fd, epoll)
+                    do_register(sock_status_W, conn, epoll)
                     manager.return_back()
-                    conn.send(chr(ord(data_str[i]) + 1).encode())
+                    send(conn, chr(ord(data_str[i]) + 1).encode())
     conn.close()
-    manager.return_back()
+    manager.finish()
 
 
 def run_server(main_greenlet):
@@ -154,7 +207,7 @@ def run_server(main_greenlet):
     socket_options = [socket.AF_INET, socket.SOCK_STREAM]
     address = ("0.0.0.0", 9090)
     with socketcontext(*socket_options) as server, \
-            epollcontext(server.fileno(), select.EPOLLIN) as epoll:
+            epollcontext(server, select.EPOLLIN) as epoll:
         server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         server.bind(address)
         server.listen(5)
@@ -166,9 +219,10 @@ def run_server(main_greenlet):
         server_fd = server.fileno()
 
         while True:
-            events = epoll.poll(1)
+            events = epoll.poll(timeout=1)
 
-            for fileno, event in events:
+            for sock, event in events:
+                fileno = sock.fileno()
                 if fileno == server_fd:
                     connection, client_address = server.accept()
                     connection.setblocking(0)
@@ -178,15 +232,12 @@ def run_server(main_greenlet):
                         main_greenlet, serve_connection)
                     connections[fd] = work_greenlet
                     work_greenlet.start(connection, fd, epoll)
+                elif (event & (select.EPOLLIN | select.EPOLLOUT)):
+                    work_greenlet = connections[fileno]
+                    work_greenlet.continue_work()
 
-                elif event & select.EPOLLIN:
-                    work_greenlet = connections[fileno]
-                    work_greenlet.continue_work()
-                elif event & select.EPOLLOUT:
-                    work_greenlet = connections[fileno]
-                    work_greenlet.continue_work()
-                else:
-                    print("event ", event)
+
+
 
 
 if __name__ == '__main__':
